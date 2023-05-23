d0df77804ad3862564fa7666f481141245d95905
LuizGMMoschetta <luizzztavo@gmail.com>
2023-05-23 14:20:33 -0300

    Função de entrada de valores adicionada

---
+++ main.py
@@ -0,0 +1,12 @@
+def gether_data():
+    n1 = int(input("Primeiro Valor: "))
+    n2 = int(input("Primeiro Valor: "))
+
+    return n1,n2
+
+def main():
+    print(2*2)
+    return None
+
+if __name__ == '__main__':
+    main()
